09ae869ae1f5eb75e02d26ac14a20c075ad6f749
magicman <Matfey02@outlook.com>
2025-04-18 16:03:03 +0300

    Start

---
+++ Snake/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Timers;
+
+namespace Snake;
+class Program
+{
+    public static bool _isGameRunning = true;
+    public static int _schetchik = 0;
+    public const int _shirina = 10;
+    public const int _visota = 10;
+    public static FueldType[,] Fueld = new FueldType[_visota, _shirina];
+    public static Food _food = new Food();
+    public static Snake _snake = new Snake(new Point(_shirina/2,_visota/3), Direction.Right);
+
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < _visota; i++)
+        {
+            for (int j = 0; j < _shirina; j++)
+            {
+                Fueld[i, j] = FueldType.Null;
+            }
+        }
+        Fueld[_snake.Body[0].X, _snake.Body[0].Y] = FueldType.Snake;
+        FoodGeneration();
+        Deistvie();
+    }
+
+
+    public static void Deistvie()
+    {
+        do
+        {
+            _snake.Move(Fueld);
+            PrintFueld();
+            Thread.Sleep(1000);
+            Console.Clear();
+        } while (_isGameRunning);
+
+
+    }
+
+    public static void PrintFueld()
+    {
+        int i = 0;
+        foreach(FueldType kletca in Fueld)
+        {
+            switch( kletca)
+            {
+                case FueldType.Null:
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    break;
+                case FueldType.Food:
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case FueldType.Snake:
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    break;
+            }
+            Console.Write("  ");
+            Console.ResetColor();
+            Console.Write(" ");
+            i++;
+            if (i == _shirina)
+            {
+                i = 0;
+                Console.WriteLine();
+            }
+        }
+            
+    }
+
+    public static void FoodGeneration()
+    {
+        bool IsNull = false;
+        do
+        {
+            _food.RndGeneration(_shirina,_visota);
+            if (Fueld[_food.Position.Y, _food.Position.X] == FueldType.Null)
+            {                        
+                IsNull = true;       
+                Fueld[_food.Position.Y, _food.Position.X] = FueldType.Food;
+            }
+        } while (IsNull == false);
+
+    }
+}
+
+
+public enum FueldType
+{
+    Null,
+    Food,
+    Snake
+}
---
+++ Snake/Snake.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Snake
+{
+	public class Snake
+	{
+		public List<Point> Body { get; set; }
+        public Direction Directeon { get; set; }
+
+		public Snake(Point point, Direction direction)
+		{
+            Directeon = direction;
+            Body = new List<Point> {point};
+		}
+
+        public void Move(FueldType[,] Fueld)
+        {
+            Fueld[Body[0].X, Body[0].Y] = FueldType.Null;
+            Body.Insert(0, GetNextPosition());
+            Fueld[Body[0].X, Body[0].Y] = FueldType.Snake;
+        }
+
+        public Point GetNextPosition()
+        {
+            switch (Directeon)
+            {
+                case Direction.Left:
+                    return new Point(Body[0].X, Body[0].Y-1);
+
+                case Direction.Right:
+                    return new Point(Body[0].X, Body[0].Y+1);
+
+                case Direction.Up:
+                    return new Point(Body[0].X-1, Body[0].Y);
+
+                case Direction.Down:
+                    return new Point(Body[0].X+1, Body[0].Y);
+
+                default:
+                    return new Point(Body[0].X, Body[0].Y);
+            }
+        }
+
+
+        public bool ChekColision(int wight, int len)
+        {
+            if (Body[0].X == 10)
+            {
+                return true;
+            }
+            return true;
+        }
+
+    }
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
+
